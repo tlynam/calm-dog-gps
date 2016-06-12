aabb1f53c8fc3bcf7ac951d301d425ffe8d68704
Todd Lynam <TLynam@gmail.com>
2016-06-12 11:22:58 -0700

    Fix nil bug

--- app/controllers/raspberry_pis_controller.rb
+++ app/controllers/raspberry_pis_controller.rb
@@ -38,12 +38,15 @@ class RaspberryPisController < ApplicationController
 
     phone_params = raspberry_pi_params.dup["phones_attributes"]
 
-    phone_params.each do |phone|
-      if phone[1]["icloud_password"].try(:blank?)
-        phone[1].delete("icloud_password")
+    if phone_params
+      phone_params.each do |phone|
+        if phone[1]["icloud_password"].try(:blank?)
+          phone[1].delete("icloud_password")
+        end
       end
+      non_phone_params.merge(phones_attributes: phone_params)
+    else
+      raspberry_pi_params
     end
-
-    non_phone_params.merge(phones_attributes: phone_params)
   end
 end
