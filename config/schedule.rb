96726e87a7af3e06b3bd70bb64554c6a6d379a4e
Todd Lynam <TLynam@gmail.com>
2016-06-13 17:10:29 -0700

    Run rake within prod env

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,7 @@ require File.expand_path('../..//config/environment.rb', __FILE__)
 interval = RaspberryPi.first.interval
 
 job_type :rbenv_rake, 'export PATH="$HOME/.rbenv/bin:$PATH"; eval "$(rbenv init -)";
-                           cd :path && bin/rake :task --silent :output'
+                           cd :path && RAILS_ENV=production bin/rake :task --silent :output'
 
 set :output, "~/cron_log.log"
 
